5bc9a4ac3f26b550df2008dbb086f5a1d6e8f703
devbyyou <lyy.pro@gmail.com>
2023-12-26 19:42:34 +0100

    Ajout du status 440

--- app/middlewares/authorize.js
+++ app/middlewares/authorize.js
@@ -3,36 +3,36 @@ const config = require('../config');
 const roles = require('../roles');
 
 const authorize = (allowedRoles) => (req, res, next) => {
-  // Récupération du token depuis les en-têtes de la requête
-  const token = req.header('Authorization');
+   // Récupération du token depuis les en-têtes de la requête
+   const token = req.header('Authorization');
 
-  // Vérification de la présence du token
-  if (!token) {
-    return res.status(401).json({ message: 'Accès non autorisé. Token manquant.' });
-  }
+   // Vérification de la présence du token
+   if (!token) {
+      return res.status(440).json({ message: 'Accès non autorisé. Token manquant.' });
+   }
 
-  try {
-    // Vérification et décodage du token
-    const decodedToken = jwt.verify(token, config.secretKey);
+   try {
+      // Vérification et décodage du token
+      const decodedToken = jwt.verify(token, config.secretKey);
 
-    // Vérification du rôle de l'utilisateur
-    if (!Object.values(roles).includes(decodedToken.role)) {
-      return res.status(403).json({ message: 'Accès interdit. Rôle utilisateur incorrect.' });
-    }
+      // Vérification du rôle de l'utilisateur
+      if (!Object.values(roles).includes(decodedToken.role)) {
+         return res.status(440).json({ message: 'Accès interdit. Rôle utilisateur incorrect.' });
+      }
 
-    // Vérification des rôles autorisés
-    if (!allowedRoles.includes(decodedToken.role)) {
-      return res.status(403).json({ message: 'Accès interdit. Rôle utilisateur incorrect.' });
-    }
+      // Vérification des rôles autorisés
+      if (!allowedRoles.includes(decodedToken.role)) {
+         return res.status(440).json({ message: 'Accès interdit. Rôle utilisateur incorrect.' });
+      }
 
-    // Ajout des informations utilisateur au req pour un accès ultérieur
-    req.user = decodedToken;
+      // Ajout des informations utilisateur au req pour un accès ultérieur
+      req.user = decodedToken;
 
-    // Autorisation accordée, passer à la prochaine middleware
-    next();
-  } catch (error) {
-    return res.status(403).json({ message: 'Accès interdit. Token invalide.' });
-  }
+      // Autorisation accordée, passer à la prochaine middleware
+      next();
+   } catch (error) {
+      return res.status(440).json({ message: 'Accès interdit. Token invalide.' });
+   }
 };
 
 module.exports = authorize;
--- app/middlewares/validateQRMiddleware.js
+++ app/middlewares/validateQRMiddleware.js
@@ -2,35 +2,35 @@
 const { Seance } = require('../models');
 
 async function validateQRMiddleware(req, res, next) {
-    try {
-        // Récupérer le QR Code scanné depuis la requête
-        const scannedQRCode = req.body.qrCode; // adapter cela à structure de requête
+   try {
+      // Récupérer le QR Code scanné depuis la requête
+      const scannedQRCode = req.body.qrCode; // adapter cela à structure de requête
 
-        // Récupérer l'ID de l'équipe et de la séance depuis les paramètres de la requête
-        const { id: equipeId, seanceId } = req.params;
+      // Récupérer l'ID de l'équipe et de la séance depuis les paramètres de la requête
+      const { id: equipeId, seanceId } = req.params;
 
-        // Récupérer la séance en cours depuis la base de données
-        const currentSeance = await Seance.findOne({
-            where: {
-                id: seanceId,
-                equipeId,
-                qrCode: scannedQRCode,
-            },
-        });
+      // Récupérer la séance en cours depuis la base de données
+      const currentSeance = await Seance.findOne({
+         where: {
+            id: seanceId,
+            equipeId,
+            qrCode: scannedQRCode,
+         },
+      });
 
-        // Vérifier si la séance avec le QR Code scanné existe et est associée à l'équipe
-        if (!currentSeance) {
-            return res.status(400).json({ message: 'QR Code invalide pour la séance en cours.' });
-        }
+      // Vérifier si la séance avec le QR Code scanné existe et est associée à l'équipe
+      if (!currentSeance) {
+         return res.status(400).json({ message: 'QR Code invalide pour la séance en cours.' });
+      }
 
-        // Ajouter les informations de la séance à la requête pour un accès ultérieur
-        req.currentSeance = currentSeance;
+      // Ajouter les informations de la séance à la requête pour un accès ultérieur
+      req.currentSeance = currentSeance;
 
-        // Passer au middleware suivant
-        next();
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+      // Passer au middleware suivant
+      next();
+   } catch (error) {
+      res.status(500).json({ error: error.message });
+   }
 }
 
 module.exports = { validateQRMiddleware };
